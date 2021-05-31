a21f71537ac77587cc266bba65003bd048d0e976
Allen Day <allenday@allenday.com>
2021-06-01 00:07:33 +0800

    streamline

--- src/components/PublicKeyTweet/index.tsx
+++ src/components/PublicKeyTweet/index.tsx
@@ -23,27 +23,14 @@ const PublicKeyTweet: React.FC = ({
   return (
     <>
       <Row>
-        <S.TextRow
-          bold
-          fontSize={24}
-          lSpacing={1}
-          mTop={30}
-        >
-          {tweetMessageText}
-        </S.TextRow>
+        <S.TextRow bold color="#00a000" mTop={5}>{rememberAttachText}</S.TextRow>
       </Row>
-      <S.RowTweetMessage>
-        <S.TextRow>{tweetMessage}</S.TextRow>
-      </S.RowTweetMessage>
       <S.RowTweet>
         <TwitterShareButton
           url={' '}
           options={{ text: tweetMessage, size: 'large' }}
         />
       </S.RowTweet>
-      <Row>
-        <S.TextRow bold color="#00a000" mTop={5}>{rememberAttachText}</S.TextRow>
-      </Row>
     </>
   )
 }
