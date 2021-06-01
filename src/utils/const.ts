c03227b478a3ce5d09686dd3c568c79761cee5d2
Allen Day <allenday@allenday.com>
2021-06-01 10:23:33 +0800

    typo

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -5,5 +5,5 @@ export const LINKS = {
   discord: "https://discord.gg/dogecoin",
   facebook: "https://facebook.com/OfficialDogecoin",
   subreddit: "https://www.reddit.com/r/dogeducation",
-  github: "https://github.com/allenday/proofofdog",
+  github: "https://github.com/allenday/proofof.dog",
 }
